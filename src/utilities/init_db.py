0248fd68019acb328b67583351167cd38007f095
Abhinav Mallick <abhinavmallick831@gmail.com>
2021-05-01 17:11:46 +0530

    ...

--- src/utilities/init_db.py
+++ src/utilities/init_db.py
@@ -2,6 +2,9 @@ import db_connection as db
 
 connection = db.DbConnection().get_connection()
 
+with open('tweet_schema.sql') as f:
+    connection.executescript(f.read())
+
 cur = connection.cursor()
 
 cur.execute("INSERT INTO tweets (tweet_data, tweet_url)"
